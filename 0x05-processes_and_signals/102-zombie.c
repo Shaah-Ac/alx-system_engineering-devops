97ea58e68b106753a8fec447efc076e94226ffa0
Sharon Achieng <otienosharon51@gmail.com>
2024-03-07 00:11:25 -0800

    signals

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,7 +1,52 @@
-#!/usr/bin/env bash
-# display 'To infinity and beyond' indefinitely,
-while "true"
-do
-	echo "To infinity and neyond"
-	sleep 2
-done
+/*
+ * File: 102-zombie.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Run an infinite while loop.
+ *
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Creates five zombie processes.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t pid;
+	char count = 0;
+
+	while (count < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			count++;
+		}
+		else
+			exit(0);
+	}
+
+	infinite_while();
+
+	return (EXIT_SUCCESS);
+}
