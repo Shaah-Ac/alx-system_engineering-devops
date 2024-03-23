8352e62a4e3179e15c8e863eebb6d6ab62ab211b
Sharon Achieng <otienosharon51@gmail.com>
2024-03-23 12:10:39 -0700

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #create a fle in /tmp
 
-file { '/tmp/school':
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet',
+file { '/tmp/school':	
+ mode => '0744',
+ owner => 'www-data',
+ group => 'www-data',
+ content => 'I love Puppet',
 }
